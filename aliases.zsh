3c7671ee488248111833dfdf6a653dd7d0e3c344
Rainy <geekrainy@gmail.com>
2021-02-26 11:46:24 +0800

    Comment git alias

--- aliases.zsh
+++ aliases.zsh
@@ -1,3 +1,3 @@
 alias brewbackup='brew bundle dump --force'
 alias signapp='codesign --force --deep --sign -'
-alias git='function _git() { case $1 in clone) git $@;; *) git $@;; esac };_git'
+#alias git='function _git() { case $1 in clone) git $@;; *) git $@;; esac };_git'
